82f4d92c1ac658372927bb496b1d63596816d8be
Your Name <you@example.com>
2025-11-23 15:51:10 -0800

    Finished Chaos Game project

--- code/ChaosGame.cpp
+++ code/ChaosGame.cpp
@@ -1,4 +1,8 @@
 // Include important C++ libraries here
+//Name: Dayanna Perez
+
+#include <cstdlib>
+#include <ctime>
 #include <SFML/Graphics.hpp>
 #include <SFML/Audio.hpp>
 #include <iostream>
@@ -11,13 +15,29 @@ using namespace std;
 
 int main()
 {
+
+	srand(static_cast<unsigned>(time(nullptr)));
+
 	// Create a video mode object
 	VideoMode vm(1920, 1080);
 	// Create and open a window for the game
 	RenderWindow window(vm, "Chaos Game!!", Style::Default);
 	
+	Font font;
+		if (!font.loadFromFile("Marxiana.ttf")) {
+			cout << "Error loading font.." << endl;
+		}
+	
+	Text text;
+	text.setFont(font);
+	text.setCharacterSize(30);
+	text.setFillColor(Color::Magenta);
+	text.setPosition(20.f, 20.f);
+	text.setString(" Welcome . . . click   3   vertices,   then   a   starting   point!");
+	
 	vector<Vector2f> vertices;
 	vector<Vector2f> points;
+
 
 	while (window.isOpen())
 	{
@@ -41,7 +61,9 @@ int main()
 			    std::cout << "the left button was pressed" << std::endl;
 			    std::cout << "mouse x: " << event.mouseButton.x << std::endl;
 			    std::cout << "mouse y: " << event.mouseButton.y << std::endl;
-	
+				
+				Vector2f clickPos(static_cast<float>(event.mouseButton.x), static_cast<float>(event.mouseButton.y));
+				
 			    if(vertices.size() < 3)
 			    {
 				vertices.push_back(Vector2f(event.mouseButton.x, event.mouseButton.y));
@@ -50,6 +72,8 @@ int main()
 			    {
 				///fourth click
 				///push back to points vector
+				points.push_back(clickPos);
+				cout << "Starting point: (" << clickPos.x << ", " << clickPos.y << ")" << endl;
 			    }
 			}
 		    }
@@ -70,6 +94,19 @@ int main()
 		    ///select random vertex
 		    ///calculate midpoint between random vertex and the last point in the vector
 		    ///push back the newly generated coord.
+			int framePoints = 100;
+
+			for (int i = 0; i < framePoints; i++){
+				int index = rand() % 3;
+				Vector2f vertex = vertices[index];
+				Vector2f lastPoint = points.back();
+
+				Vector2f newPoint;
+				newPoint.x = (lastPoint.x + vertex.x) / 2.0;
+				newPoint.y = (lastPoint.y + vertex.y) / 2.0;
+
+				points.push_back(newPoint);
+			}
 		}
 	
 		/*
@@ -82,10 +119,20 @@ int main()
 		{
 		    RectangleShape rect(Vector2f(10,10));
 		    rect.setPosition(Vector2f(vertices[i].x, vertices[i].y));
-		    rect.setFillColor(Color::Blue);
+		    rect.setFillColor(Color::Green);
+			rect.setOrigin(5,5);
 		    window.draw(rect);
 		}
+		for (int i = 0; i < points.size(); i++){
+			CircleShape circ;
+			circ.setRadius(1);
+			circ.setPosition(Vector2f(points[i].x, points[i].y));
+		    circ.setFillColor(Color::Magenta);
+			circ.setOrigin(1,1);
+		    window.draw(circ);
+		}
 		///TODO:  Draw points
+		window.draw(text);
 		window.display();
 	}
 }
